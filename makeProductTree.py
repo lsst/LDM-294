48d000ff7c664a7a64bb16974beee033f1ee2c6f
William O'Mullane <womullan@lsst.org>
2018-01-04 10:40:21 -0700

    spaces not tab

--- makeProductTree.py
+++ makeProductTree.py
@@ -42,7 +42,7 @@ def constructTree(fin):
                 continue
         count = count + 1
 
-	part = line
+        part = line
         prod = Product(part[0], part[1], part[2], part[3], part[4], part[6],
                        part[7], part[8], part[9])
         # print("Product:" + prod.id + " name:" + prod.name + " parent:"
